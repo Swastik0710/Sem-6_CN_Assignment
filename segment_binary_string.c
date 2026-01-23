ba156e8df89ebb821be8ba2a54cb5c79a48ddabd
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2026-01-23 11:02:16 +0530

    Implement padding for binary string segments
    Added functionality to pad binary string with leading zeros if its length is not a multiple of the segment length.

--- segment_binary_string.c
+++ segment_binary_string.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
     char str[50];
     int seg, len, count;
+
     printf("Enter a string of 0's and 1's: ");
     scanf("%s", str);
+
     printf("Enter segment length: ");
     scanf("%d", &seg);
+
     len = strlen(str);
+
     while (len % seg != 0) {
         for (int i = len; i > 0; i--) {
             str[i] = str[i - 1];
@@ -16,6 +21,7 @@ int main() {
         len++;
         str[len] = '\0';
     }
+
     for (int i = 0; i < len; i += seg) {
         count = 0;
         printf("Segment: ");
@@ -26,5 +32,6 @@ int main() {
         }
         printf("  -> Number of 1's: %d\n", count);
     }
+
     return 0;
 }
